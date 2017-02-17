a6d0c16c710900d69fc6cfdfea4eb3785bec00bf
Anamul Hoque Shihab <ashihab@itcollege.ee>
2017-02-17 11:59:40 +0200

    index.php file added
    its just the prototype of the intended webpage...

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Introduction to this guy's website">
+    <title>This goes into the titlebar</title>
+    <link rel="css/style.css" type="text/css"/>
+    <script type="text/javascript" src="js/main.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no"/><!-- Disable zoom on smartphone -->
+  </head>
+  <body>
+    <header>
+      Shihab's Store !
+    </header>
+    <nav>
+      Navigation links go here
+    </nav>
+    <section>
+      Product items go here
+    </section>
+    <article>
+      The actual content goes here
+      <?php echo "This, is hello from PHP!"; ?>
+    </article>
+    <aside>
+      Context specific links go here
+    </aside>
+    <footer>
+      Copyright none :)
+    </footer>
+  </body>
+ 
+</html>
+
+</body>
+</html>
